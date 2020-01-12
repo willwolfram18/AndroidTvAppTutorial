f7a9d13e9ddb97143773edd7e0e21065b2229739
willwolfram18 <willwolfram18@gmail.com>
2020-01-11 22:53:42 -0800

    Basic browse fragment

--- app/src/main/java/com/willwolfram/androidtvapptutorial/MainFragment.java
+++ app/src/main/java/com/willwolfram/androidtvapptutorial/MainFragment.java
@@ -14,5 +14,21 @@ public class MainFragment extends BrowseFragment {
         Log.i(Tag, "onActivityCreated");
 
         super.onActivityCreated(savedInstanceState);
+
+        setupUIElements();
+    }
+
+    private void setupUIElements() {
+        setTitle("Hello Fire TV!");
+
+        setHeadersState(HEADERS_ENABLED);
+        setHeadersTransitionOnBackEnabled(true);
+
+        setBrandColor(getColor(R.color.fastlane_background));
+        setSearchAffordanceColor(getColor(R.color.search_opaque));
+    }
+
+    private int getColor(int colorId) {
+        return getResources().getColor(colorId);
     }
 }
